07a3649cf42e9bb9ee83999f415d606e6f03f25e
Lucky Jha <jhalucky61@gmail.com>
2026-07-06 18:10:59 +0530

    show_rows using show

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.config.spark_session import get_spark_session
 from src.extract.read_csv import read_csv
-from src.extract.explore_df import print_schema, show_columns, describe_data, count_rows
+from src.extract.explore_df import print_schema, show_columns, describe_data, count_rows, show_rows
 
 spark = get_spark_session()
 customer_df = read_csv(spark, "data/raw/customers.csv")
@@ -8,5 +8,6 @@ customer_df = read_csv(spark, "data/raw/customers.csv")
 # print(f"spark version : {spark.version}")
 # print(show_columns(customer_df))
 # print(describe_data(customer_df))
-print(count_rows(customer_df))
+# print(count_rows(customer_df))
+print(show_rows(customer_df, 10))
 spark.stop()
--- src/extract/explore_df.py
+++ src/extract/explore_df.py
@@ -1,20 +1,23 @@
 def print_schema(df):
 
-    return df.printSchema()
+    df.printSchema()
 
 def show_columns(df):
 
-    return df.columns()
+    return df.columns
 
 def count_rows(df):
 
     return df.count()
 
+def show_rows(df, rows=10):
+    
+    df.show(rows, truncate=False)
+
 
 def describe_data(df):
     
-    return df.describe()
-
+    return df.describe().show()
 
 
 
