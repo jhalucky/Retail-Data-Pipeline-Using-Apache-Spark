6829ca67a6b9ee2f127c0c66c73f8abb051fa721
Lucky Jha <jhalucky61@gmail.com>
2026-07-06 23:48:24 +0530

    duplicates vaidator

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from src.extract.read_csv import read_csv
 from src.extract.explore_df import print_schema, show_columns, describe_data, count_rows, show_rows
 from src.experiments.lazy_evaluation import lazy_evaluation_demo
 from src.validate.missing import validate_missing_values
+from src.validate.duplicates import validate_duplicate_rows
 
 spark = get_spark_session()
 customer_df = read_csv(spark, "data/raw/customers.csv")
@@ -10,10 +11,11 @@ customer_df = read_csv(spark, "data/raw/customers.csv")
 # print(f"spark version : {spark.version}")
 # print(show_columns(customer_df))
 # print(describe_data(customer_df))
-# print(count_rows(customer_df))
+print(count_rows(customer_df))
 # print(show_rows(customer_df, 10))
 # print(validate_missing_values(customer_df))
 
-customer_df.dropDuplicates()
+print(validate_duplicate_rows(customer_df))
+print(count_rows(customer_df))
 # lazy_evaluation_demo(customer_df)
 spark.stop()
--- src/validate/duplicates.py
+++ src/validate/duplicates.py
@@ -1,4 +1,12 @@
 def validate_duplicate_rows(df):
 
-    duplicate_rows = df.
+    total_rows = df.count()
+
+    unique_rows = df.dropDuplicates().count()
+
+    duplicate_rows = total_rows - unique_rows
+
+    print(f"Duplicate rows: {duplicate_rows}")
+
+    return df
 
