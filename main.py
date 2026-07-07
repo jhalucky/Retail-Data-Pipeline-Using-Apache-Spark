7602984ff044e0d34102b0db24682ee727c3c690
Lucky Jha <jhalucky61@gmail.com>
2026-07-07 09:33:07 +0530

    validator for duplicate primary keys

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from src.extract.read_csv import read_csv
 from src.extract.explore_df import print_schema, show_columns, describe_data, count_rows, show_rows
 from src.experiments.lazy_evaluation import lazy_evaluation_demo
 from src.validate.missing import validate_missing_values
-from src.validate.duplicates import validate_duplicate_rows
+from src.validate.duplicates import validate_duplicate_rows, validate_duplicate_primary_keys
 
 spark = get_spark_session()
 customer_df = read_csv(spark, "data/raw/customers.csv")
@@ -15,7 +15,8 @@ print(count_rows(customer_df))
 # print(show_rows(customer_df, 10))
 # print(validate_missing_values(customer_df))
 
-print(validate_duplicate_rows(customer_df))
+# print(validate_duplicate_rows(customer_df))
+print(validate_duplicate_primary_keys(customer_df, ["customer_id"]))
 print(count_rows(customer_df))
 # lazy_evaluation_demo(customer_df)
 spark.stop()
